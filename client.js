b7d1bd420866dcae81b3fd26cee0439e35bcd164
Vicente Ferrer <vic798@gmail.com>
2021-02-18 08:47:27 +0100

    Add ruby example.

--- client.js
+++ client.js
@@ -3,3 +3,5 @@ const { metacall_load_from_memory, metacall } = require('metacall');
 metacall_load_from_memory('rpc', 'http://localhost:3051');
 
 console.log(metacall('sum', 3, 4)); // 7
+
+console.log(metacall('mult', 3, 4)); // 12
---
+++ scripts/mult.rb
@@ -0,0 +1,3 @@
+def mult(a, b)
+    return a * b
+end
